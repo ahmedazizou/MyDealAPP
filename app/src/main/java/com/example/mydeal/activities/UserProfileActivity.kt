c356c9977025d4d2fb010abe00d1c33c017d3778
Ahmed A <68253091+ahmedazizou@users.noreply.github.com>
2020-11-25 22:39:00 +0100

    Make layout for profile edit

---
+++ app/src/main/java/com/example/mydeal/activities/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mydeal.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mydeal.R
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
