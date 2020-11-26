3f9ddb2756f8f75761a368fd1f05303f7eaf2300
Ahmed A <68253091+ahmedazizou@users.noreply.github.com>
2020-11-26 20:13:53 +0100

    take care of the different fragments

--- app/src/main/java/com/example/mydeal/ui/fragments/ProductsFragment.kt
+++ app/src/main/java/com/example/mydeal/ui/fragments/ProductsFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 
 import com.example.mydeal.R
 
-class HomeFragment : Fragment() {
+class ProductsFragment : Fragment() {
 
     //private lateinit var homeViewModel: HomeViewModel
 
@@ -19,7 +19,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         //homeViewModel =ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val root = inflater.inflate(R.layout.fragment_products, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         //homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = ""
